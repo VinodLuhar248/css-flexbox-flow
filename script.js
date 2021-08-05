cc268b4dea5303caf18380bc6b50d8d9d198fd6c
VinodLuhar248 <vinod.luhar248@gmail.com>
2021-08-05 12:02:41 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log('hello!')
+//flex-flow
+function setFlexboxFlowClass(value) {
+  console.log('setFlexboxFlow value :: ' + value);
+  var mainContainer = $('#flexFlowClass');
+  mainContainer.removeClass().addClass('container');
+  // //adding selected class to container
+  mainContainer.addClass(value);
+}
